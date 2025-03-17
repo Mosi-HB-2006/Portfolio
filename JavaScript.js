1a9cf3dba59a5844db39fdf5eaf2578664f76b07
10-Tyger-17 <mosi.hickman2006@gmail.com>
2025-03-17 23:09:54 +0100

    3-17-25

--- JavaScript.js
+++ JavaScript.js
@@ -1,23 +1,76 @@
-window.onload = function(){
-    const button=document.getElementById("contactButton");
-    button.onmouseover = function () {changeColorHover(button,'#9fab96','#536779')};
-    button.onmouseout = function () {changeColorHover(button,'#A7B49E','#5C7285')};
+window.onload = function () {
+    const button = document.getElementById("contactButton");
+    button.onmouseover = function () { changeColorHoverInto(button) };
+    button.onmouseout = function () { changeColorHoverOut(button) };
 
-    const option1=document.getElementsByClassName("option1")[0];
-    option1.onmouseover = function () {changeColorHover(option1,'#9fab96','#536779')};
-    option1.onmouseout = function () {changeColorHover(option1,'#A7B49E','#5C7285')};
+    const option1 = document.getElementsByClassName("option1")[0];
+    option1.onmouseover = function () { changeColorHoverInto(option1) };
+    option1.onmouseout = function () { changeColorHoverOut(option1) };
 
-    const option2=document.getElementsByClassName("option2")[0];
-    option2.onmouseover = function () {changeColorHover(option2,'#9fab96','#536779')};
-    option2.onmouseout = function () {changeColorHover(option2,'#A7B49E','#5C7285')};
+    const option2 = document.getElementsByClassName("option2")[0];
+    option2.onmouseover = function () { changeColorHoverInto(option2) };
+    option2.onmouseout = function () { changeColorHoverOut(option2) };
 
-    const option3=document.getElementsByClassName("option3")[0];
-    option3.onmouseover = function () {changeColorHover(option3,'#9fab96','#536779')};
-    option3.onmouseout = function () {changeColorHover(option3,'#A7B49E','#5C7285')};
+    const option3 = document.getElementsByClassName("option3")[0];
+    option3.onmouseover = function () { changeColorHoverInto(option3) };
+    option3.onmouseout = function () { changeColorHoverOut(option3) };
 }
 
+function changeColorHoverInto(element) {
+    element.style.color = '#9fab96';
+    element.style.backgroundColor = '#536779';
+    zoomIn(element);
+}
 
-function changeColorHover(elemento, colText, colBg) {
-    elemento.style.color=colText;
-    elemento.style.backgroundColor=colBg;
+function changeColorHoverOut(element) {
+    element.style.color = '#A7B49E';
+    element.style.backgroundColor = '#5C7285';
+    zoomOut(element);
+}
+
+let isZooming = false;
+let originalFontSize = null;
+let id = null;
+
+function zoomIn(element) {
+    if (!isZooming) {
+        isZooming = true;
+        if (!originalFontSize) {
+            originalFontSize = Math.ceil((parseInt(window.getComputedStyle(element).fontSize) / window.innerWidth) * 100);
+        }
+        let size = originalFontSize;
+        const sizeMax = originalFontSize + 0.7;
+        clearInterval(id);
+        id = setInterval(frame, 5);
+
+        function frame() {
+            if (size >= sizeMax) {
+                clearInterval(id);
+                isZooming = false;
+            } else {
+                size += 0.1;
+                element.style.fontSize = size + 'vw';
+            }
+        }
+    }
+}
+
+function zoomOut(element) {
+    if (!isZooming) {
+        isZooming = true;
+        let size = (parseInt(window.getComputedStyle(element).fontSize) / window.innerWidth) * 100;
+        const sizeMin = originalFontSize;
+        clearInterval(id);
+        id = setInterval(frame, 5);
+
+        function frame() {
+            if (size <= sizeMin) {
+                clearInterval(id);
+                isZooming = false;
+            } else {
+                size -= 0.1;
+                element.style.fontSize = size + 'vw';
+            }
+        }
+    }
 }
